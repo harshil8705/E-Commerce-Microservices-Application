c035dc01a961aeea4564a97a1007c239f93e0652
HarshilChampaneri <161130419+HarshilChampaneri@users.noreply.github.com>
2025-12-24 15:02:46 +0530

    Configured Zipkin and fixed System bugs

--- Services/gateway/src/main/java/com/harshilInfotech/gateway/GatewayApplication.java
+++ Services/gateway/src/main/java/com/harshilInfotech/gateway/GatewayApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		app.setDefaultProperties(java.util.Map.of("server.port", "8822"));
+		app.run(args);
 	}
 
 }
